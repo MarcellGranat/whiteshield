8bf32c4ac87e87b5c1af638de852375c0ab2952f
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2023-01-06 00:07:35 +0100

    debug with try

--- 02-translate.py
+++ 02-translate.py
@@ -3,21 +3,43 @@ import json
 import sys
 import pandas
 from googletrans import Translator
+import os
+from tqdm import tqdm
+import time
 
 translator = Translator()
+non_english_df = pandas.read_csv("foreign_language.csv")
 
-language_df = pandas.read_csv("language.csv")
+if os.path.isdir('translation') == False:
+  os.mkdir('translation')
 
-non_english_df = language_df.loc[language_df['language'] != 'english']
+if os.path.isdir('translation'):
+  n_translated_sentences = len(os.listdir('translation')) * 1000
+  non_english_df = non_english_df.iloc[n_translated_sentences:]
 
 out=[]
+file_name = 1
 
-for element in range(0, len(non_english_df['text'])):
-    t = non_english_df['text'][element]
-    translation = translator.translate(t, dest='en', src = "ar") # FIXME
-    out.append(translation)
-    
-df = pandas.DataFrame(out, columns=["translation"])
-df.to_csv('translation.csv', index=False)
-
+for element in tqdm(range(0, len(non_english_df['text']))):
+  if element % 20 == 0:
+      time.sleep(1)
+  t = non_english_df.iloc[element,1]
+  translation = '___still nothing___'
+  n_error = 0
+  while translation == '___still nothing___':
+    try:
+        translation = translator.translate(t, dest='en', src = "ar").text
+    except:
+        n_error += 1
+        print("Error! Wait " + str(60*n_error) + "sec...")
+        time.sleep(60*n_error)
+        if n_error > 3:
+          translation = "___fatal_error___"
+          print("Fatal error :/")
+  out.append(translation)
+  if (element + 1) % 1000 == 0:
+    df = pandas.DataFrame(out, columns=["translation"])
+    df.to_csv('translation/translation' + str(file_name) + '.csv', index=False)
+    file_name += 1
+    out = []
 

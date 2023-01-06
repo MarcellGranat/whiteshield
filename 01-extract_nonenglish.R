c0495a9dc466804126f0805000fea741266897d9
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2023-01-06 23:09:43 +0100

    google translator failed to work

--- 01-extract_nonenglish.R
+++ 01-extract_nonenglish.R
@@ -3,15 +3,11 @@ foreign_language_df <- job_post_data_df %>%
   unnest_tokens(output = "text", JobDescription, token = "sentences") %>% 
   filter(str_detect(str_sub(text, start = 3), "^[^a-zA-Z 0-9().,'-=!?:’%&]"))
 
-foreign_language_df |> 
-  mutate(text = str_remove_all(text, "\u00100")) |> 
-  mutate(line = ((row_number() - 1) / n()) %/% .5 + 1, .before = everything()) |> 
-  group_by(line) |> 
-  group_walk(~ {
-    .x |> 
-      select(- line) |> 
-      openxlsx::write.xlsx(str_c("foreign_language_", first(.x$line), ".xlsx"))
-  }, .keep = TRUE)
 
+board |> 
+  pin_write(
+    x = foreign_language_df,
+    name = "foreign_language"
+  )
+  
 write.csv(foreign_language_df, "foreign_language.csv")
-

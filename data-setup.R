db9b696d1a4d88c8a02914b613362c2087691153
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2023-01-06 23:09:08 +0100

    bug in reading from shared folder with external account

--- data-setup.R
+++ data-setup.R
@@ -1,19 +1,3 @@
-od <- Microsoft365R::get_business_onedrive(tenant = "common")
-# use `Microsoft365R::get_personal_onedrive()` 
-# if you want to access the data with it
-
-if (od$properties$owner$user$displayName == "Granát Marcell Péter") {
-  board <- board_ms365(
-    drive = od, 
-    path = "whiteshield"
-  )
-} else {
-  shared_items <- od$list_shared_files()
-  folder_to_board <- shared_items$remoteItem[[which(shared_items$name == "whiteshield")]]
-  if (!exists("folder_to_board")) message("You need access to the data")
-  board <- board_ms365(od, folder_to_board)
-}
-
 ilo_stat_df <- pin_read(board, "ilo_stat_df")
 job_post_data_df <- pin_read(board, "job_post_data_df")
 unemployed_df <- pin_read(board, "unemployed_df")

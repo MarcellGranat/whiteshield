b2abc9da2e92af46482d31956c7a019276b553d0
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2023-01-07 21:22:59 +0100

    function to collect the requirements

---
+++ 06-job-requirements.R
@@ -0,0 +1,49 @@
+job_post_sentences <- pin_read(board, "job_post_translated") |> 
+  transmute(line = row_number(), JobDescription) |> 
+  mutate(
+    JobDescription = str_replace_all(JobDescription, "[.]{2}", "."),
+    # .. caused by remerging after translation >> incorrect tokenizing for senteces
+    JobDescription = str_replace_all(JobDescription, "[.]", ". "), # ensure tokenizing
+    JobDescription = str_replace_all(JobDescription, "[.]  ", ". "), 
+    JobDescription = str_replace_all(JobDescription, " {2,}", " "),
+    # upper case after .
+    JobDescription = str_replace_all(JobDescription, set_names(str_c(". ", LETTERS), str_c("[.] ", letters))) 
+  ) |> 
+  unnest_tokens("sentence", JobDescription, token = "sentences") 
+
+examples_l <- list()
+
+find_requirement <- function(x, example_lines = 1:3) {
+  
+  requirement <- x[[1]]
+  pattern_for_sentence <- str_flatten(x[[2]], "|")
+  pattern <- str_flatten(x[[3]], "|")
+  
+  detected <- job_post_sentences |> 
+    filter(str_detect(sentence, pattern_for_sentence)) |> 
+    filter(str_detect(sentence, pattern))
+  
+  examples <- detected |> 
+    slice(example_lines) |> 
+    pull(sentence)
+  
+  examples_l <<- append(examples_l, list(pattern_for_sentence, pattern, examples))
+  
+  detected |> 
+    transmute(
+      line, 
+      requirement = x[[1]],
+      pattern_for_sentence = str_flatten(x[[2]], collapse = " or "),
+      pattern = str_flatten(x[[3]], collapse = " or "),
+      value = str_extract(sentence, pattern)
+    )
+  
+}
+
+requirement_l <- list(
+  list("experience", "experience", "\\d{1,2}"),
+  list("male", "only female", "female")
+) |> 
+  progress_map(find_requirement)
+
+

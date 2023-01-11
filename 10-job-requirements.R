1d9e8ab94ad539688ccfadbc5d1f6d74e15399ec
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2023-01-11 21:57:37 +0100

    degree

--- 10-job-requirements.R
+++ 10-job-requirements.R
@@ -1,33 +1,13 @@
 job_post_translated <- pin_read(board, "job_post_translated")
 
-around <- \(x) str_c("\\W{0,}\\w{0,}\\W{0,}\\w{0,}\\W{0,}\\w{0,}", x ,"\\w{0,}\\W{0,}\\w{0,}\\W{0,}\\w{0,}\\W{0,}\\w{0,}\\W{0,}\\w{0,}\\W{0,}")
 
-job_post_translated %>% 
+# Age minimum -------------------------------------------------
+
+age_minimum_df <- job_post_translated |> 
   transmute(
     line = row_number(),
     text = str_remove_all(JobDescription, "\\t|\\n")
   ) |> 
-  head(5) |> 
-  mutate(
-    experience_sent = str_extract(text, around("experience")),
-    experience_num = map(experience_sent, ~ str_extract_all(., pattern = "\\d{1,2}")[[1]]),
-    experience_min = map2_dbl(experience_num, experience_sent, function(num, sent) {
-      if (length(num) > 0) {
-        as.numeric(num[1])
-      } else {
-        NA
-      }
-    }
-    ),
-    experience_min = ifelse(experience_min > 30, NA, experience_min)
-  )
-
-job_post_translated |> 
-  sample_n(1000) |> 
-  transmute(
-    line = row_number(),
-    text = str_remove_all(JobDescription, "\\t|\\n")
-    ) |> 
   mutate(
     age_1 = str_extract(text, "[0-9]{2} years[ A-z]* age"),
     age_2 = str_extract(text, "[0-9]{2} years old and below"),
@@ -38,8 +18,60 @@ job_post_translated |>
   select(- text) |> 
   pivot_longer(-1) |> 
   drop_na() |> 
-  mutate(
-    value = str_extract(value, "\\d{1,2}"),
-    value = ifelse(value < 20 | value > 60, NA, value)
+  transmute(
+    line,
+    age_min = str_extract(value, "\\d{1,2}"),
+    age_min = as.numeric(age_min),
+    age_min = ifelse(age_min < 20 | age_min > 60, NA, age_min)
   )
+
+# Experience, degree ------------------------------------------
+
+around <- \(x) str_c("\\W{0,}\\w{0,}\\W{0,}\\w{0,}\\W{0,}\\w{0,}", x ,"\\w{0,}\\W{0,}\\w{0,}\\W{0,}\\w{0,}\\W{0,}\\w{0,}\\W{0,}\\w{0,}\\W{0,}")
+
+titles <- pin_read(board, "unemployed_df") |> 
+  pull(Education) |> 
+  unique()
+
+job_post_translated %>% 
+  transmute(
+    line = row_number(),
+    text = str_remove_all(JobDescription, "\\t|\\n")
+  ) |> 
+  head(5) |> 
+  left_join(age_minimum_df, by = "line") |> 
+  mutate(
+    experience_sent = str_extract(text, around("experience")),
+    experience_min = map_dbl(experience_sent, function(sent) {
+      num = str_extract_all(sent, pattern = "\\d{1,2}")[[1]]
+      
+      if (length(num) > 0) {
+        as.numeric(num[1])
+      } else {
+        NA
+      }
+    }
+    ),
+    experience_min = ifelse(experience_min > 30, NA, experience_min),
+    degree = str_extract(text, "[ A-z]+ [A-z]+ degree [ A-z]+ "),
+    degree_title = str_extract(
+      degree, 
+      paste("(?i)", c(titles, " ba "," ma ", " mba", "bsc", "undergraduate", "university", "college"), sep = "", collapse = "|")
+    ),
+    min_degree = case_when(
+      str_detect(degree_title, paste(c("(?i)diploma"), sep = "", collapse = "|")) ~ "Diploma",
+      str_detect(degree_title, paste(c("(?i)bachelor", "(?i)ba", "(?i)bsc", "university", "undergraduate", "college"), sep = "", collapse = "|")) ~ "Bachelor",
+      str_detect(degree_title, paste(c("(?i)master", "(?i)ma", "(?i)msc", "(?i)mba"), sep = "", collapse = "|")) ~ "Master",
+      str_detect(degree_title, paste(c("(?i)doctorate", "(?i)phd"), sep = "", collapse = "|")) ~ "Doctorate",
+      TRUE ~ NA_character_
+    ),
+    degree_year = case_when(
+      min_degree == "Diploma" ~ 2,
+      min_degree == "Bachelor" ~ 3, 
+      min_degree == "Master" ~ 5, 
+      min_degree == "Doctorate" ~ 8,
+      TRUE ~ 0
+    )
+  )
+
 
